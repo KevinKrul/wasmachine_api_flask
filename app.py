f1c58ddf564719e46b8b90171e9c5d16a3a10702
kevin <ky.krul@gmail.com>
2021-08-03 18:31:33 +0200

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from datetime import datetime
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+import json
+from bson import json_util
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ['SECRET']
+mongo = PyMongo(app)
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    starttijd = mongo.db.tijden.find().sort("start", -1).limit(2)
+    output = [{'starttijd' : doc['start'] } for doc in starttijd]
+    return jsonify(output)
+
+
+@app.route('/start', methods=['GET'])
+def start():
+    mongo.db.tijden.insert_one({'start': datetime.utcnow()});
+    return  {'result' : 'Created successfully'}
